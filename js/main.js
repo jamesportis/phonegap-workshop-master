3938c5314fce14feb6996795a6ca09da945e7795
James Portis <jamesportis@yahoo.com>
2015-04-23 20:03:24 -0700

    last

--- js/main.js
+++ js/main.js
@@ -1,14 +1,38 @@
 var app = {
-
-   
 	
-	showAlert: function (message, title) {
+		showAlert: function (message, title) {
     if (navigator.notification) {
         navigator.notification.alert(message, null, title, 'OK');
     } else {
         alert(title ? (title + ": " + message) : message);
     }
 },
+
+   registerEvents: function() {
+	   $(window).on('hashchange', $.proxy(this.route, this));
+    var self = this;
+    // Check of browser supports touch events...
+    if (document.documentElement.hasOwnProperty('ontouchstart')) {
+        // ... if yes: register touch event listener to change the "selected" state of the item
+        $('body').on('touchstart', 'a', function(event) {
+            $(event.target).addClass('tappable-active');
+        });
+        $('body').on('touchend', 'a', function(event) {
+            $(event.target).removeClass('tappable-active');
+        });
+    } else {
+        // ... if not: register mouse events instead
+        $('body').on('mousedown', 'a', function(event) {
+            $(event.target).addClass('tappable-active');
+        });
+        $('body').on('mouseup', 'a', function(event) {
+            $(event.target).removeClass('tappable-active');
+        });
+    }
+	
+},
+	
+
 
 slidePage: function(page) {
  
@@ -68,41 +92,18 @@ route: function() {
         });
     }
 },
-
-registerEvents: function() {
-    var self = this;
-    // Check of browser supports touch events...
-    if (document.documentElement.hasOwnProperty('ontouchstart')) {
-        // ... if yes: register touch event listener to change the "selected" state of the item
-        $('body').on('touchstart', 'a', function(event) {
-            $(event.target).addClass('tappable-active');
-        });
-        $('body').on('touchend', 'a', function(event) {
-            $(event.target).removeClass('tappable-active');
-        });
-    } else {
-        // ... if not: register mouse events instead
-        $('body').on('mousedown', 'a', function(event) {
-            $(event.target).addClass('tappable-active');
-        });
-        $('body').on('mouseup', 'a', function(event) {
-            $(event.target).removeClass('tappable-active');
-        });
-    }
-	$(window).on('hashchange', $.proxy(this.route, this));
-},
-
-
 	
 
-   initialize: function() {
-    var self = this;
-    this.detailsURL = /^#employees\/(\d{1,})/;
-    this.registerEvents();
-    this.store = new LocalStorageStore(function() {
-        self.route();
-    });
-}
+  initialize: function() {
+        var self = this;
+        this.detailsURL = /^#employees\/(\d{1,})/;
+        this.registerEvents();
+        this.store = new MemoryStore(function() {
+            self.route();
+        });
+    }
+
 };
+
 
 app.initialize();
